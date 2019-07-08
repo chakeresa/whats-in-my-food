078a425aae92d06c056486e5a381fa0ed0bcbcc0
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-08 09:45:31 -0600

    Add UsdaFoodApiService

--- app/facades/food_index_facade.rb
+++ app/facades/food_index_facade.rb
@@ -1,15 +1,24 @@
 class FoodIndexFacade
   attr_reader :ingredient
-  
+
   def initialize(ingredient)
     @ingredient = ingredient
   end
 
   def foods
-    
+    foods_hash = service.foods_with_ingredient(@ingredient)
+    foods_hash.map do |food_data|
+      Food.new(food_data)
+    end
   end
 
   def food_count
     foods.count
+  end
+
+  private
+
+  def service
+    UsdaFoodApiService.new
   end
 end
---
+++ app/services/usda_food_api_service.rb
@@ -0,0 +1,5 @@
+class UsdaFoodApiService
+  def foods_with_ingredient(ingredient)
+    
+  end
+end
