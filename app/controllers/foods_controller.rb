a28fcf465c3170275ae3bad8a91dfe521b0757f6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-08 09:36:30 -0600

    Add FoodIndexFacade

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,7 @@
 class FoodsController < ApplicationController
   def index
-    
+    render locals: {
+      facade: FoodIndexFacade.new
+    }
   end
 end
---
+++ app/facades/food_index_facade.rb
@@ -0,0 +1,13 @@
+class FoodIndexFacade
+  def ingredient
+    
+  end
+
+  def foods
+    
+  end
+
+  def food_count
+    foods.count
+  end
+end
