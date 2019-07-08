bab54e1cb46a124598721db4cbe947908314282f
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-08 10:28:58 -0600

    Turn on vcr

--- spec/services/usda_food_api_service_spec.rb
+++ spec/services/usda_food_api_service_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe UsdaFoodApiService do
   it 'fetches a list of foods with a particular ingredient' do
-    WebMock.allow_net_connect!
-    VCR.turn_off!
-    ingredient = "sweet potatoes"
-    service = UsdaFoodApiService.new
-    expect(service.foods_with_ingredient(ingredient)).to have_key("list")
-    expect(service.foods_with_ingredient(ingredient)["list"]["total"]).to eq(531)
-    # "group", "name", "ndbno", "ds", "manu"
-    expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("group")
-    expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("name")
-    expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("ndbno")
-    expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("ds")
-    expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("manu")
+    VCR.use_cassette('api_fetches_list_of_foods_by_ingredient', record: :new_episodes) do
+      ingredient = "sweet potatoes"
+      service = UsdaFoodApiService.new
+      expect(service.foods_with_ingredient(ingredient)).to have_key("list")
+      expect(service.foods_with_ingredient(ingredient)["list"]["total"]).to eq(531)
+      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("group")
+      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("name")
+      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("ndbno")
+      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("ds")
+      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("manu")
+    end
   end
 end
