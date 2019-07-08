670f3837c71d589dc054d83f44d1c970af81e258
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-08 10:54:03 -0600

    Refactor usda_food_api_service_spec

--- spec/services/usda_food_api_service_spec.rb
+++ spec/services/usda_food_api_service_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe UsdaFoodApiService do
   it 'fetches a list of foods with a particular ingredient' do
     VCR.use_cassette('api_fetches_list_of_foods_by_ingredient', record: :new_episodes) do
       ingredient = "sweet potatoes"
-      service = UsdaFoodApiService.new
-      expect(service.foods_with_ingredient(ingredient)).to have_key("list")
-      expect(service.foods_with_ingredient(ingredient)["list"]["total"]).to eq(531)
-      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("group")
-      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("name")
-      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("ndbno")
-      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("ds")
-      expect(service.foods_with_ingredient(ingredient)["list"]["item"].first).to have_key("manu")
+      result = UsdaFoodApiService.new.foods_with_ingredient(ingredient)
+      expect(result).to have_key("list")
+      expect(result["list"]["total"]).to eq(531)
+      expect(result["list"]["item"].first).to have_key("group")
+      expect(result["list"]["item"].first).to have_key("name")
+      expect(result["list"]["item"].first).to have_key("ndbno")
+      expect(result["list"]["item"].first).to have_key("ds")
+      expect(result["list"]["item"].first).to have_key("manu")
     end
   end
 end
