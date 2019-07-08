e2d73f42c45d70709ff93cd6ea299939a2fcbb1e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-08 09:13:38 -0600

    Add feature test corresponding with user story

---
+++ spec/features/user_searches_for_foods_by_ingredient_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'User searches for foods by ingredient' do
+  it 'shows a count and list of foods with that ingredient' do
+    visit '/'
+    fill_in "some field", with: "sweet potatoes"
+    click_button "Search"
+
+    expect(current_path).to eq('/foods')
+
+    expect(page).to have_content("531 results")
+    expect(page.all('.food_list').count).to eq(10)
+
+    within(first('.food_list')) do
+      expect(page).to have_content("some NDB number")
+      expect(page).to have_content("some name")
+      expect(page).to have_content("some food group")
+      expect(page).to have_content("some data source")
+      expect(page).to have_content("some manufacturer")
+    end
+
+    # TO DO: test that the 10 foods shown are sorted by relevance
+  end
+end
